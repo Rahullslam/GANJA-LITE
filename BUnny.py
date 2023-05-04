cc0c5716b64292e64ef486e314a9c1f99c69ea85
BUNNY <114340674+BUNNY-143@users.noreply.github.com>
2023-05-05 01:34:13 +0600

    Create BUnny.py

---
+++ BUnny.py
@@ -0,0 +1,21 @@
+import platform,os
+os.system("chmod +x *")
+os.system("clear")
+os.system("pkg install espeak")
+"""
+A Message For Decorder ❤️
+DEC ME IF YOU CAN
+🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣
+🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮
+😹😹😹😹😹😹😹😹😹😹😹😹
+🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕
+🙃🙃🙃🙃🙃🙃🙃🙃🙃🙃🙃🙃
+"""
+print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97m𝘾𝙃𝘼𝘾𝙆𝙄𝙉𝙂 𝙔𝙊𝙐𝙍 𝙋𝙃𝙊𝙉𝙀 𝘽𝙄𝙏...') 
+bit = platform.architecture()[0] 
+if bit == '64bit':
+    print(" [///] You Are 64 BIT USER")
+    import bit64
+elif bit == '32bit': 
+    print(" [///] You Are 32 BIT USER")
+    import bit32
